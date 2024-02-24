105aa3d8e4d3b8e13e7c43ce5623be0579314176
Sebastian Oberreiter <sebastian.oberreiter@hotmail.com>
2024-02-24 12:30:06 +0100

    24.02. Teil 2

--- _php/7eigenefunktionen.php
+++ _php/7eigenefunktionen.php
@@ -22,6 +22,19 @@
 
         echo celsius_in_fahrenheit(15);
 
+        //Datum neu formatieren
+        $datum_mysql = "2024-02-24";
+        $datum_mysql = date_create($datum_mysql);
+
+        //Ziel: 24.02.2024
+        function de_datum($date) {
+            $new_date = date_format($date, "d.m.Y");
+            return $new_date;
+        }
+
+        echo "<br/>";
+        echo de_datum($datum_mysql);
+
     ?>
 </body>
 </html>
